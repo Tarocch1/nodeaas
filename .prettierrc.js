1c7494fac2f3ed979f49a5975d090d25f86c29c8
Tarocch1 <huiben.fang@gmail.com>
2021-09-20 11:25:00 +0800

    chore: 🎨 prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,1 @@
-module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: false,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-  endOfLine: 'lf',
-}
+module.exports = require('@tarocch1/eslint-config/.prettierrc')
